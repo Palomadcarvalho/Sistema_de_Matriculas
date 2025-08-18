11bc8cdd359f56287d13b6d97b18da786229d2d0
Paloma Dias de Carvalho <103902312+Palomadcarvalho@users.noreply.github.com>
2025-08-18 17:34:53 -0300

    Update Disciplina.java

--- 02. Códigos/Disciplina.java
+++ 02. Códigos/Disciplina.java
@@ -1,1 +1,20 @@
+import java.util.List;
+
+public class Disciplina {
+    private String codigo;
+    private String nome;
+    private int creditos;
+    private int vagasMax = 60;
+    private boolean ativa;
+    private List<Aluno> alunosMatriculados;
+
+    public boolean verificarAtivacao() {
+
+        return false;
+    }
+
+    public List<Aluno> getAlunos() {
+        return alunosMatriculados;
+    }
+}
 
