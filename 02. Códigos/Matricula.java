abd66b19bd6ad91d701ab11ae071d125d9ff2c60
Paloma Dias de Carvalho <103902312+Palomadcarvalho@users.noreply.github.com>
2025-08-18 17:42:49 -0300

    Update Matricula.java
    código java da matrícula

--- 02. Códigos/Matricula.java
+++ 02. Códigos/Matricula.java
@@ -1,1 +1,15 @@
+import java.util.Date;
+
+public class Matricula {
+    private Date data;
+    private String tipo;
+
+    public Date getData() {
+        return data;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+}
 
