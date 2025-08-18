3dffe00417caf75d25d87defcb1b133f632a809e
Paloma Dias de Carvalho <103902312+Palomadcarvalho@users.noreply.github.com>
2025-08-18 17:43:45 -0300

    Update PeriodoMatricula.java
    código java do período da matrícula

--- 02. Códigos/PeriodoMatricula.java
+++ 02. Códigos/PeriodoMatricula.java
@@ -1,1 +1,11 @@
+import java.util.Date;
 
+public class PeriodoMatricula {
+    private Date dataInicio;
+    private Date dataFim;
+    private boolean isAberto;
+
+    public boolean isAberto() {
+        return isAberto;
+    }
+}
