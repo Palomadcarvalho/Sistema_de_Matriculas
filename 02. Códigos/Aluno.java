c2e2f0c557be6bd016c89b37758b4ccb4a3cda1a
Paloma Dias de Carvalho <103902312+Palomadcarvalho@users.noreply.github.com>
2025-08-18 17:31:40 -0300

    Update Aluno.java

--- 02. Códigos/Aluno.java
+++ 02. Códigos/Aluno.java
@@ -1,1 +1,23 @@
+import java.util.List;
+
+public class Aluno {
+    private int id;
+    private String nome;
+    private String matricula;
+    private List<Disciplina> disciplinasObrigatorias;
+    private List<Disciplina> disciplinasOptativas;
+    private List<Curso> cursos;
+
+    public void inscreverDisciplina(Disciplina d) {
+
+    }
+
+    public void cancelarInscricao(Disciplina d) {
+
+    }
+
+    public List<Curso> getCursos() {
+        return cursos;
+    }
+}
 
