04f36e4e61474eed689cd4b7f3d05b0385d562a6
Paloma Dias de Carvalho <103902312+Palomadcarvalho@users.noreply.github.com>
2025-08-18 17:38:06 -0300

    Update Professor.java
    código

--- 02. Códigos/Professor.java
+++ 02. Códigos/Professor.java
@@ -1,1 +1,17 @@
+import java.util.List;
 
+public class Professor {
+    private int id;
+    private String nome;
+    private String siape;
+    private List<Disciplina> disciplinas;
+    private List<Curso> cursos;
+
+    public List<Disciplina> getDisciplinas() {
+        return disciplinas;
+    }
+
+    public List<Curso> getCursos() {
+        return cursos;
+    }
+}
