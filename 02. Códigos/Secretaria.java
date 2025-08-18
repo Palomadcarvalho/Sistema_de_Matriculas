5c300bce6034899275986f950982f742594d7406
Paloma Dias de Carvalho <103902312+Palomadcarvalho@users.noreply.github.com>
2025-08-18 17:40:42 -0300

    Update Secretaria.java
    código java da Secretaria

--- 02. Códigos/Secretaria.java
+++ 02. Códigos/Secretaria.java
@@ -1,1 +1,22 @@
+public class Secretaria {
+    private int id;
+
+    public void gerarCurriculo() {
+    }
+
+    public void cadastrarDisciplina(Disciplina d) {}
+    public void atualizarDisciplina(Disciplina d) {}
+    public void removerDisciplina(String codigo) {}
+    public Disciplina consultarDisciplina(String codigo) { return null; }
+
+    public void cadastrarProfessor(Professor p) {}
+    public void atualizarProfessor(Professor p) {}
+    public void removerProfessor(String siape) {}
+    public Professor consultarProfessor(String siape) { return null; }
+
+    public void cadastrarAluno(Aluno a) {}
+    public void atualizarAluno(Aluno a) {}
+    public void removerAluno(String matricula) {}
+    public Aluno consultarAluno(String matricula) { return null; }
+}
 
