452d1ca1a42d4f1d49bd3b3b51b69ec3e4627349
Paloma Dias de Carvalho <103902312+Palomadcarvalho@users.noreply.github.com>
2025-08-18 17:36:07 -0300

    Update Curso.java

--- 02. Códigos/Curso.java
+++ 02. Códigos/Curso.java
@@ -1,1 +1,20 @@
+import java.util.List;
 
+public class Curso {
+    private int id;
+    private String nome;
+    private int cargaHoraria;
+    private List<Disciplina> disciplinas;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public List<Disciplina> getDisciplinas() {
+        return disciplinas;
+    }
+}
